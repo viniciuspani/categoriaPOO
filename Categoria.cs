109cee04885273d3cd36530b2ba9157f5bc8587c
Vinicius Pani <viniciuspani@hotmail.com>
2024-10-22 21:06:01 -0300

    Categoria Adicionada.

---
+++ Categoria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Categoria
+    {
+        private static int _ultimoId = 0;
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+
+        public Categoria(string nome)
+        {
+            Id = ++_ultimoId; // Incrementa o ID ao criar uma nova instância
+            Nome = nome;
+        }
+
+        public static List<Categoria> IniciarCategorias()
+        {
+            return
+            [
+                new Categoria("Smartphones"),
+                new Categoria("Tablets"),
+                new Categoria("Acessórios"),
+                new Categoria("Wearables"),
+                new Categoria("Fones de Ouvido")
+            ];
+        }
+
+        public void ExibirCategoria()
+        {
+            Console.WriteLine($"{Id}: {Nome}");
+        }
+    }
+}
+
